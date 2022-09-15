dd7d58367e64efbf7beed9c05c72c9acf4be91d1
AtchayaPraba <atchyapraba93@gmail.com>
2022-09-15 19:50:20 +0530

    Check for CI/CD pipeline

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET","POST"])
+
+def check_CICD_pipeline ():
+    return "Check for CI/CD pipeline"
+
+if __name__ == "__main__":
+    app.run(debug=True)
