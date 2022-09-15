53ad2004357dce67e1117886e2ff19935bfb44e3
AtchayaPraba <atchyapraba93@gmail.com>
2022-09-15 20:05:56 +0530

    changes in CI/CD pipeline

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/", methods=["GET","POST"])
 
 def check_CICD_pipeline ():
-    return "Check for CI/CD pipeline"
+    return "Check for CI/CD pipeline. Check for 'changes' in CI/CD pipeline."
 
 if __name__ == "__main__":
     app.run(debug=True)
